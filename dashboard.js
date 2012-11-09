cd374363cf2c176f70c204208fb9d0092a233e8d
Max Garrick <mgarrick@uci.edu>
2012-11-09 13:21:16 -0800

    Changed graphite URL from port 80 to 8080

--- dashboard.js
+++ dashboard.js
@@ -3,28 +3,28 @@
   description = {
 
     "# Requests": {
-      source: "http://stats.oit.uci.edu/render/?from=-1day&format=json&_salt=1352477600.709&target=stats_counts.recruit.requests",
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352477600.709&target=stats_counts.recruit.requests",
       TimeSeries: {
         parent: '#g1-1',
 	title: "# requests"
       }
     },
     "Bundles Downloaded": {
-      source: "http://stats.oit.uci.edu/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.application.bundle.cache.*.*",
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.application.bundle.cache.*.*",
       TimeSeries: {
         parent: '#g1-2',
 	title: "# bundles"
       }
     },
     "Applicant Contact Us": {
-      source: "http://stats.oit.uci.edu/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.contact.*.*",
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.contact.*.*",
       TimeSeries: {
         parent: '#g2-1',
 	title: "# applicant contact us"
       }
     },
     "Reference Contact Us": {
-      source: "http://stats.oit.uci.edu/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.contact.*.*",
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.contact.*.*",
       TimeSeries: {
         parent: '#g2-2',
 	title: "# reference contact us"
