b7d4c83e69b767525b406911bbac797a64ae10af
Max Garrick <mgarrick@uci.edu>
2012-11-09 16:46:00 -0800

    Added a few more metrics

--- dashboard.js
+++ dashboard.js
@@ -9,6 +9,7 @@
 	title: "# requests"
       }
     },
+
     "Bundles Downloaded": {
       source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.application.bundle.cache.*.*",
       TimeSeries: {
@@ -16,18 +17,60 @@
 	title: "# bundles"
       }
     },
+
     "Applicant Contact Us": {
       source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.contact.*.*",
       TimeSeries: {
-        parent: '#g2-1',
+        parent: '#g1-3',
 	title: "# applicant contact us"
       }
     },
+
     "Reference Contact Us": {
       source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.contact.*.*",
       TimeSeries: {
-        parent: '#g2-2',
+        parent: '#g2-1',
 	title: "# reference contact us"
+      }
+    },
+
+    "Applicant Login": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.login.success",
+      TimeSeries: {
+        parent: '#g2-2',
+	title: "# applicant logins"
+      }
+    },
+
+    "Applicant Login Failures": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.login.failure",
+      TimeSeries: {
+        parent: '#g2-3',
+	title: "# applicant login failures"
+      }
+    },
+
+    "Reference Login": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.login.success",
+      TimeSeries: {
+        parent: '#g3-1',
+	title: "# reference logins"
+      }
+    },
+
+    "Reference Login Failures": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.login.failure.*",
+      TimeSeries: {
+        parent: '#g3-2',
+	title: "# reference login failures"
+      }
+    },
+
+    "Applicant Forgot Password": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.forgot.*",
+      TimeSeries: {
+        parent: '#g3-3',
+	title: "# applicant forgot password"
       }
     },
   };
