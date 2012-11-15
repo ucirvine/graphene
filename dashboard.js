57c4d28843f09b84da138bd987ee1579e34a8831
Max Garrick <mgarrick@uci.edu>
2012-11-15 08:04:28 -0800

    Added recruitments, applicants, and reference deltas.  Also updated targets to match what Recruit is now using

--- dashboard.js
+++ dashboard.js
@@ -18,26 +18,34 @@
       }
     },
 
-    "Applicant Contact Us": {
-      source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.contact.*.*",
+    "Recruitments": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.recruitment.delta",
       TimeSeries: {
         parent: '#g1-3',
-	title: "# applicant contact us"
+	title: "recruitments delta"
       }
     },
 
-    "Reference Contact Us": {
-      source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.contact.*.*",
+    "Applicants": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.delta",
       TimeSeries: {
         parent: '#g2-1',
-	title: "# reference contact us"
+	title: "applicants delta"
+      }
+    },
+
+    "References": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1day&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.delta",
+      TimeSeries: {
+        parent: '#g2-2',
+	title: "references delta"
       }
     },
 
     "Applicant Login": {
       source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.login.success",
       TimeSeries: {
-        parent: '#g2-2',
+        parent: '#g2-3',
 	title: "# applicant logins"
       }
     },
@@ -45,7 +53,7 @@
     "Applicant Login Failures": {
       source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.login.failure",
       TimeSeries: {
-        parent: '#g2-3',
+        parent: '#g3-1',
 	title: "# applicant login failures"
       }
     },
@@ -53,7 +61,7 @@
     "Reference Login": {
       source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.login.success",
       TimeSeries: {
-        parent: '#g3-1',
+        parent: '#g3-2',
 	title: "# reference logins"
       }
     },
@@ -61,7 +69,7 @@
     "Reference Login Failures": {
       source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.login.failure.*",
       TimeSeries: {
-        parent: '#g3-2',
+        parent: '#g3-3',
 	title: "# reference login failures"
       }
     },
@@ -69,8 +77,24 @@
     "Applicant Forgot Password": {
       source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.forgot.*",
       TimeSeries: {
-        parent: '#g3-3',
+        parent: '#g4-1',
 	title: "# applicant forgot password"
+      }
+    },
+
+    "Applicant Contact Us": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.applicant.contact.*.*",
+      TimeSeries: {
+        parent: '#g4-2',
+	title: "# applicant contact us"
+      }
+    },
+
+    "Reference Contact Us": {
+      source: "http://stats.oit.uci.edu:8080/render/?from=-1hour&format=json&_salt=1352478526.16&target=stats_counts.recruit.reference.contact.*.*",
+      TimeSeries: {
+        parent: '#g4-3',
+	title: "# reference contact us"
       }
     },
   };
